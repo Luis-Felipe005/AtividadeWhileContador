e766c22d6857eb88f35e501176d7299e624b2402
Luis-Felipe005 <135838158+Luis-Felipe005@users.noreply.github.com>
2023-06-14 17:29:37 -0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int contador = 0;
+
+            while ( contador < 5 )
+            {
+                Console.WriteLine("olá");
+                contador = contador + 1;
+
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
